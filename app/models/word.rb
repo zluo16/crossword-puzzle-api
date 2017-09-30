717f1d2f2314f76b0cbaacced28e55d7bdb976c8
Mike <zluo@alumni.berklee.edu>
2017-09-30 10:56:50 -0400

    added a create word method that pulls words from api

--- app/models/word.rb
+++ app/models/word.rb
@@ -10,4 +10,32 @@ class Word < ApplicationRecord
     end
   end
 
+  def self.create_words(offset)
+    data = []
+    offset = offset
+    15000.times do
+      d = RestClient.get("http://jservice.io/api/clues.json?offset=#{offset}")
+      parsed = JSON.parse(d)
+      parsed.each do |object|
+        if object['answer'] != nil && object['question'] != nil
+          data.push(object)
+        end
+      end
+      offset += parsed.length
+    end
+
+
+    single_words = data.select{ |clue| clue["answer"].split(" ").length < 2 }
+
+    db_words = single_words.select do |word|
+      if word["value"]
+        word["answer"].length == 5 || word["answer"].length == 3 || word["answer"].length == 8
+      end
+    end
+
+
+    db_words.each{ |word| Word.create(answer: word["answer"], question: word["question"], value: word["value"]) }
+
+  end
+
 end
