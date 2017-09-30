5447082e3a07f2da3a60a9be3f81378ea5ba6f00
Mike <zluo@alumni.berklee.edu>
2017-09-30 15:26:37 -0400

    changed random gen method

--- app/models/puzzle.rb
+++ app/models/puzzle.rb
@@ -94,10 +94,7 @@ class Puzzle < ApplicationRecord
 
     words.each{ |word| self.add_word(word[:word], word[:alignment], word[:boxId]) }
 
-    if self.grid_boxes.find{ |b| b.boxId === 32 }.className === 'empty'
-      self.empty_grid
-      self.crossword_random_gen
-    elsif self.puzzle_words.length < 13
+    while self.grid_boxes.find{ |b| b.boxId === 31 }.className === 'empty' || self.puzzle_words.length < 13
       self.empty_grid
       self.crossword_random_gen
     end
